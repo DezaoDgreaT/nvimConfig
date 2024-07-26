8517cd021538dcd91b1eed95e8c4bef669bc68b3
PADILHA BREVES <a.padilhajunior@dxc.com>
2024-07-26 20:16:26 -0300

    null-ls added

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,5 +1,5 @@
 -- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
+--https://github.com/DezaoDgreaT/nvimConfig.git  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
 return {
